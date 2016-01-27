cb8ae2eaf466b81f00611e67d9f3013d5d10a835
Edgar Matevosyan <edgar.matevosyan@dreamworks.com>
2016-01-27 11:56:18 -0800

    added json response and better comments

--- server.js
+++ server.js
@@ -33,15 +33,25 @@ server.get('/api/v1/list', function (req, res, next) {
         locale = 'locale=en_US',
         cid = 'cid=490388',
         sig = 'sig='+ newSig +'',
+
+        /*
+          * these variables need to be set from the angular call
+          * assuming in angular you keep the request but just to /api/v1/list
+          * instead and pull the json response from that...duh rest...
+        */
         destinationString = 'destinationString=Seattle,WA',
         arrivalDate = 'arrivalDate=10/10/2016',
         departureDate = 'departureDate=10/11/2016',
-        currencyCode = 'currencyCode=USD',
-        numberOfResults = 'numberOfResults=10',
         room1 = 'room1=2',
+        /*
+          * just those variables, rest can stay in server side
+        */
+
+        currencyCode = 'currencyCode=USD',
+        numberOfResults = 'numberOfResults=5',
         url = 'http://api.ean.com/ean-services/rs/hotel/v3/list?'+ apiKey +'&'+ minorRev +'&'+ locale +'&'+ cid +'&'+ sig +'&'+ destinationString +'&'+ arrivalDate +'&'+ departureDate +'&'+ currencyCode +'&'+ numberOfResults +'&'+ room1 +''
 
-    request({url:url}, function (error, response, body) {
+    request({url:url, json:true}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             //set content type to json for restify to work properly res = restify
             res.send(body);
